3f13a98ce3e774f08b272b836503bc4db8ef20f7
Kevin Chen <kchen508@gmail.com>
2012-11-12 16:45:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buttonjax::Application.config.secret_token = '354c09ca452ca10bbb8d6d5d2b3cb7c73c7514ed88d7b4fe0fddeb63c26276e8d705173c456d246ad5866cdb46ec88ce04a81e2c82b0285b5dc3422265c055b3'
